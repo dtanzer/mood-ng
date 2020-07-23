72a85702f6f4b83c283712c9fc07df833d36f63c
David Tanzer <business@davidtanzer.net>
2020-07-23 15:54:44 +0200

    Add capability to display emoji.
    $ npm install --save @ctrl/ngx-emoji-mart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { EmojiModule } from '@ctrl/ngx-emoji-mart/ngx-emoji';
+
 @NgModule({
 	declarations: [
 		AppComponent
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		AppRoutingModule,
+		EmojiModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent]
